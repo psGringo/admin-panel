2b31db25e70efeae794965c46e3b7c830ddaa174
Panteleev Stas <SOPanteleev@sberbank.ru>
2021-06-15 01:30:12 +0500

    Начало темной темы

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,5 +1,5 @@
-import React from "react";
-import classes from './MainPage.module.css';
+import React, {useEffect} from "react";
+import styles from './MainPage.module.css';
 import {Header} from "../Header/Header";
 import {Filter} from "../Filters/Filter/Filter";
 import {Table} from "../Tables/MainTable/Table/Table";
@@ -7,11 +7,21 @@ import {TableFooter} from "../Tables/MainTable/TableFooter/TableFooter";
 import {ModalFormDelete} from "../Forms/ModalFormDelete/ModalFormDelete";
 import {OrderForm} from "../Forms/RightOrderForm/OrderForm/OrderForm";
 import {ModalFormTheme} from "../Forms/ModalFormTheme/ModalFormTheme";
+import {useSelector} from "react-redux";
 
 export const MainPage = () => {
 
+    const isLightTheme = useSelector(state => state.theme.isLight)
+
+    useEffect(() => {
+        (!isLightTheme) && document.body.classList.add(styles.darkTheme);
+        return () => {
+            (!isLightTheme) && document.body.classList.remove(styles.darkTheme);
+        }
+    });
+
     return (
-            <div className={classes._}>
+            <div className={styles._}>
                 <Header />
                 <Filter />
                 <Table />
